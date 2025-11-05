fda8b92d1c57d0fb82fa029b837f3b493a902d1e
Julie Dedieu <julie.dedieu1@gmail.com>
2025-11-05 10:15:31 +0100

    Installed twig-pack and created first template homepage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
+        $starshipCount = 4457;
+        $myShip = [
+            'name' => 'Rocinante',
+            'class' => 'Battle',
+            'captain' => 'Jim Holden',
+            'status' => 'under maintenance',
+        ];
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarship' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
     }
 }
